631d9cd1440f5985881dc14c3bd22eab9eb57254
Afsal1 <afsalgafoor2000@gmail.com>
2020-10-12 16:20:27 +0530

    all completed final1

--- student_management_system/settings.py
+++ student_management_system/settings.py
@@ -85,17 +85,19 @@ WSGI_APPLICATION = 'student_management_system.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME' : 'student_management_system',
-        'USER' : 'root',
-        'PASSWORD' : '12345',
-        'HOST' : 'localhost',
-        'PORT' : '3306'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+
     }
 }
 
 
-
+# 'ENGINE': 'django.db.backends.mysql',
+# 'NAME' : 'student_management_system',
+# 'USER' : 'root',
+# 'PASSWORD' : '12345',
+# 'HOST' : 'localhost',
+# 'PORT' : '3306'
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
