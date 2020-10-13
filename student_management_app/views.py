576bb882b7121e5e3e0de12519e25c38fcb91a20
Afsal1 <afsalgafoor2000@gmail.com>
2020-10-13 16:28:24 +0530

    all complete7

--- student_management_app/views.py
+++ student_management_app/views.py
@@ -23,15 +23,15 @@ def doLogin(request):
         return HttpResponse("<h2>Method Not Allowed</h2>")
 
     else:
-        captcha_token=request.POST.get("g-recaptcha-response")
-        cap_url="https://www.google.com/recaptcha/api/siteverify"
-        cap_secret="6LfZmNMZAAAAAFE0sKsUXt1kUjB_8EmQ2ZiBbV5A"
-        cap_data={"secret":cap_secret,"response":captcha_token}
-        cap_server_response=requests.post(url=cap_url,data=cap_data)
-        cap_json=json.loads(cap_server_response.text)
-        if cap_json['success']==False:
-            messages.error(request,"Invalid Captcha Try Again")
-            return HttpResponseRedirect("/")
+        # captcha_token=request.POST.get("g-recaptcha-response")
+        # cap_url="https://www.google.com/recaptcha/api/siteverify"
+        # cap_secret="6LfZmNMZAAAAAFE0sKsUXt1kUjB_8EmQ2ZiBbV5A"
+        # cap_data={"secret":cap_secret,"response":captcha_token}
+        # cap_server_response=requests.post(url=cap_url,data=cap_data)
+        # cap_json=json.loads(cap_server_response.text)
+        # if cap_json['success']==False:
+        #     messages.error(request,"Invalid Captcha Try Again")
+        #     return HttpResponseRedirect("/")
         user=authenticate(request,username=request.POST.get("email"),password=request.POST.get("password"))
         if user!=None:
             login(request,user)
